eca8748b8d686ee81ceda3e260824c2e5f57cc2f
Walt <waltlungan@gmail.com>
2022-04-10 00:26:45 -0700

    added problem at line 1

--- cw040922.js
+++ cw040922.js
@@ -1,3 +1,8 @@
+/*Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john" */
+
+
 // function nameShuffler(str){
 //   //Shuffle It
 // }
