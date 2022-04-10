0263da956476f101b8fbe6729d7be25bb6e1829a
Walt <waltlungan@gmail.com>
2022-04-09 23:39:23 -0700

    "..." 8kyu, 04-09-22

---
+++ cw040922.js
@@ -0,0 +1,8 @@
+// function nameShuffler(str){
+//   //Shuffle It
+// }
+
+//Problem: a string with two names is passed, the names need to be reversed
+//solution/pseudo: string can be split with split method on the " ", afterwards, can use reverse method to reverse the index numbers from last to first?
+
+let nameShuffler = str => str.split(' ').reverse().join(' ')
